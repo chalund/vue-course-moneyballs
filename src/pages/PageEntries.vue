8e512685dc8844cdc9b7f98ea58f9516f29d01c6
chalund <114383740+chalund@users.noreply.github.com>
2024-10-15 12:47:03 +0200

    currency formatting

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -1,26 +1,16 @@
 <template>
   <q-page>
     <div class="q-pa-md">
-      <q-list
-      bordered
-      separator
-      >
-        <q-item
-        v-for="entry in entries"
-        key="entry.id"
-        >
-
+      <q-list bordered separator>
+        <q-item v-for="entry in entries" key="entry.id">
           <q-item-section>
             {{ entry.name }}
           </q-item-section>
 
           <q-item-section side>
-           {{ entry.amount }}
+            {{ currencify(entry.amount) }}
           </q-item-section>
         </q-item>
-
-
-
       </q-list>
     </div>
   </q-page>
@@ -36,26 +26,51 @@ import { ref } from "vue";
 entries
 */
 
- const entries = ref ([
+const entries = ref([
   {
-    id: 'id1',
-    name: 'Salary',
+    id: "id1",
+    name: "Salary",
     amount: 4999.99,
   },
   {
-    id: 'id2',
-    name: 'Rent ',
+    id: "id2",
+    name: "Rent ",
     amount: -999,
   },
   {
-    id: 'id3',
-    name: 'Phone',
+    id: "id3",
+    name: "Phone",
     amount: -14.99,
   },
   {
-    id: 'id4',
-    name: 'Unknown',
+    id: "id4",
+    name: "Unknown",
     amount: 0,
   },
- ])
+
+  ])
+
+  /*
+  currencify
+  */
+  function currencify(amount) {
+    // format: "+ $ 4999.99" | "- $ 999.00"
+
+    let posNegSymbol = ''
+    if (amount > 0) {
+      posNegSymbol = '+'
+    } else if (amount < 0) {
+      posNegSymbol = '-'
+    }
+
+    const currencySymbol = '$',
+    amountPositive = Math.abs(amount),
+    amountFormatted = amountPositive.toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    return `${posNegSymbol} ${currencySymbol} ${amountFormatted}`;
+  }
+
 </script>
